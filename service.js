a00a027898388a90d7115267e958fecb420026c5
Joel Cormack <dgowellkomrak@gmail.com>
2023-05-08 22:10:04 -0600

    feature/add basic structure to service.js and basic user form

--- service.js
+++ service.js
@@ -15,22 +15,7 @@ MDS.init(function (msg) {
 
     //Do initialisation
     if (msg.event == "inited") {
-
-        //Create the DB if not exists
-        createDB(function (msg) {
-            MDS.log("SQL DB inited");
-        });
-
-        //run this function at intervals of every 15 seconds
-        setInterval(function () {
-            //Check, remove and delete expired MLS
-            removeExpiredMLS(function (msg) {
-                MDS.log("Checked for expired MLS");
-            });
-        }, 15000);
-        //21600000 6 hours
-
-        //Check rechatter messages
+        //MDS.log("dmax service inited");
     } else if (msg.event == "MAXIMA") {
 
         //Is it for dmax...
@@ -49,44 +34,22 @@ MDS.init(function (msg) {
                 //What type is this..
                 var type = json.type;
 
-                if (type == "P2P_REQUEST") {
-                    // Get the amount from the original P2P_REQUEST
+                if (type == "P2P_RESPONSE") {
+                    //create two variables for the amount and the p2pidentity
                     var amount = json.data.amount;
-                    var contact = json.data.contact;
+                    var p2pidentity = json.data.p2pidentity;
 
-                    // Send response to client via maxima, including the amount
-                    sendMaximaMessage(contact, { "type": "P2P_RESPONSE", "data": { "status": "OK", "amount": amount } }, function (msg) {
-                        MDS.log("Sent response to " + contact);
-                    });
+                    //set the static MLS
+                    //maxextra action:staticmls host: <p2pidentity> (Mx...@34.190.784.3:9001)
+                    //send money
+                    //send amount of money from form to the max wallet (hard coded address)
+                    //coinID is retiurned
+                    //send via maxima coinID
+                    //send(‘paymentConfirmation’, {coinID, clientPK})
                 }
 
-                else if (messagetype == "PAYMENT_CONFIRMATION") {
-                    // Get the coin id the client has sent
-                    var coinId = json.data.coin_id;
-
-                    // Confirm payment
-                    confirmPayment(coinId, function (msg) {
-                        var amount = msg.response.amount;
-
-                        // Add the clients permanent maxima address
-                        addPermanentAddress(publickey, function (msg) {
-                            MDS.log("Added permanent address for " + publickey);
-
-                            // Set the date that the MLS will expire
-                            setExpiryDate(amount, function (expirydate) {
-                                MDS.log("Set expire date for " + publickey);
-
-                                getP2PIdentity(function (p2p) {
-                                    var permAddress = 'MAX#' + publickey + '#' + p2p;
-                                    MDS.log('Permanent Address: ' + permAddress);
-                                    // Send response to client via maxima
-                                    sendMaximaMessage(permAddress, { "type": "EXPIRY_DATE", "data": { "status": "OK", "expiry_date": expirydate } }, function (msg) {
-                                        MDS.log("Sent response to " + publickey);
-                                    });
-                                });
-                            });
-                        });
-                    });
+                else if (messagetype == "ENDDATE_RESPONSE") {
+                    //navigate user to countdown page
                 } else {
                     MDS.log("INVALID message type in dmax server: " + messagetype);
                 }
